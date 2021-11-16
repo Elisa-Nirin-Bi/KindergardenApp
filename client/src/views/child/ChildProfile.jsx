e096ab27dc32d338b02c2e98ee98837699a8a4b1
santiag0ur <83630273+santiag0ur@users.noreply.github.com>
2021-11-16 19:27:37 +0100

    issue on child details arranged

--- client/src/views/child/ChildProfile.jsx
+++ client/src/views/child/ChildProfile.jsx
@@ -5,17 +5,31 @@ class ChildProfile extends Component {
   constructor() {
     super();
     this.state = {
-      child: null
+      id: null,
+      name: null,
+      address: null,
+      emergencyContactNumber: null,
+      parent: null
     };
   }
 
   componentDidMount() {
     getChild(this.props.match.params.id)
-      .then((child) => {
-        const childinfo = child.child;
+      .then((childinfo) => {
+        const childToShow = childinfo.child;
+        console.log('childToShow');
+        console.log(childToShow);
         this.setState(() => {
-          return { child: childinfo };
+          return {
+            id: childToShow._id,
+            name: childToShow.name,
+            address: childToShow.address,
+            emergencyContactNumber: childToShow.emergencyContactNumber,
+            parent: childToShow.parent.name
+          };
         });
+        console.log('this.stateInsideComponent');
+        console.log(this.state);
       })
       .catch((error) => {
         console.log(error);
@@ -23,13 +37,15 @@ class ChildProfile extends Component {
   }
 
   render() {
-    return this.state.child ? (
+    console.log('this.state');
+    console.log(this.state);
+    return this.state.id ? (
       <div>
-        <h2>Name: {this.state.child.name}</h2>
-        <h3>Address: {this.state.child.address}</h3>
-        <h3>Emergency Telephone: {this.state.child.emergencyContactNumber}</h3>
-        <h3>Parent: {this.state.child.parent}</h3>
-        <a href={`/child/${this.state.child._id}/upload`}>Create </a>
+        <h2>Name: {this.state.name}</h2>
+        <h3>Address: {this.state.address}</h3>
+        <h3>Emergency Telephone: {this.state.emergencyContactNumber}</h3>
+        <h3>Parent: {this.state.parent}</h3>
+        {/* <a href={`/child/${this.state.id}/upload`}>Create </a> */}
       </div>
     ) : (
       <div></div>
