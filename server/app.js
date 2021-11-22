6963e10abf8ed457e7b00ec27e595298387e0868
Elisa Barontini <nirinproductions@gmail.com>
2021-11-22 12:22:06 +0000

    localhost modified

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [ process.env.CLIENT_APP_ORIGIN, 'https://kindergardenapp.netlify.app'
+    origin: [process.env.CLIENT_APP_ORIGIN, 'http://localhost:3501/'],
     credentials: true
   })
 );
